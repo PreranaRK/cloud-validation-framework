fd96e8b4eed0960bbf9f0787b3b60e9e99ecce3b
PreranaRK <36686758+PreranaRK@users.noreply.github.com>
2020-03-09 23:12:44 +0530

    Update test_log_handler.py

--- tests/processor/logging/test_log_handler.py
+++ tests/processor/logging/test_log_handler.py
@@ -7,3 +7,34 @@ def test_getlogger():
     logger1 = getlogger()
     assert logger1 is not None
     assert id(logger) == id(logger1)
+
+    
+def test_parse_int():
+    from processor.helper.config.config_utils import parseint
+    val = parseint('23')
+    assert val == 23
+    val = parseint('ajey')
+    assert val == 0
+    val = parseint('ajey', 20)
+    assert val == 20
+
+
+def test_parse_bool():
+    from processor.helper.config.config_utils import parsebool
+    value = parsebool(5.6, True)
+    assert value == True
+    value = parsebool(True)
+    assert value == True
+    value = parsebool(0.6, True)
+    assert value == False
+
+
+def test_compare_in():
+    from processor.comparison.comparisonantlr.compare_types import compare_in
+    from processor.comparison.comparisonantlr.compare_types import EQ, NEQ, GT, GTE, LT, LTE
+    val = compare_in('hi', 'hi', EQ)
+    assert val == True
+    val = compare_in('hello', 'man', EQ)
+    assert val == False
+    val = compare_in('hi', 0, EQ)
+    assert val == False
